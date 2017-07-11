b7a234e67f06c0b7686ec67aa6da2edeee9f80c4
kotlarz <marius.kotlarz@gmail.com>
2017-07-11 16:12:39 +0200

    Add seperate threads for readers and sensors

--- commands/start.py
+++ commands/start.py
@@ -1,23 +1,30 @@
 import logging
+import threading
+
+import sys
+
+import time
 
 from app.games.implementations.ping_pong import PingPongGame
+from app.sensors import MPR121
 from app.settings import READER_VENDOR_ID, READER_PRODUCT_ID, READER_A_PHYSICAL_PATH, READER_B_PHYSICAL_PATH
 from app.readers.hid.hid_reader import HIDReader
 from app.readers.reader_listener import ReaderListener
 
+logging.basicConfig(level=logging.DEBUG)
+game = PingPongGame()
 
-def start(game_slug):
-    logging.basicConfig(level=logging.DEBUG)
 
-    game = PingPongGame()
+class CapraNFCReader(ReaderListener):
+    def handle_card_read(self, physical_path, card):
+        game.register_card(physical_path, card)
 
-    class CapraNFCReader(ReaderListener):
-        def handle_card_read(self, physical_path, card):
-            game.register_card(physical_path, card)
+    def handle_data(self, physical_path, message):
+        pass
 
-        def handle_data(self, physical_path,message):
-            pass
 
+def start_reader():
+    print('Starting reader thread')
     ports = HIDReader.find_readers(
         READER_VENDOR_ID,
         READER_PRODUCT_ID,
@@ -26,3 +33,38 @@ def start(game_slug):
     reader = HIDReader(ports)
     reader.add_read_listener(CapraNFCReader())
     reader.connect()
+
+
+def start_sensor_input():
+    print('Starting sensor thread')
+
+    cap = MPR121.MPR121()
+
+    if not cap.begin():
+        print('Error initializing MPR121.  Check your wiring!')
+        sys.exit(1)
+
+    last_touched = cap.touched()
+    while True:
+        current_touched = cap.touched()
+        # Check each pin's last and current state to see if it was pressed or released.
+        for i in range(12):
+            # Each pin is represented by a bit in the touched value.  A value of 1
+            # means the pin is being touched, and 0 means it is not being touched.
+            pin_bit = 1 << i
+            # First check if transitioned from not touched to touched.
+            if current_touched & pin_bit and not last_touched & pin_bit:
+                print('{0} touched!'.format(i))
+            # Next check if transitioned from touched to not touched.
+            if not current_touched & pin_bit and last_touched & pin_bit:
+                print('{0} released!'.format(i))
+        # Update last state and wait a short period before repeating.
+        last_touched = current_touched
+        time.sleep(0.1)
+
+
+def start(game_slug):
+    reader_thread = threading.Thread(target=start_reader)
+    reader_thread.start()
+    sensor_thread = threading.Thread(target=start_sensor_input)
+    sensor_thread.start()
